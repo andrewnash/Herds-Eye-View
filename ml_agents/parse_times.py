26d978f741cc032f6f10e382af082abab5346e94
Andrew Nash <anash@mun.ca>
2023-08-09 10:56:00 -0230

    Paper Ready Push Block models and timers

--- ml_agents/parse_times.py
+++ ml_agents/parse_times.py
@@ -1,19 +1,22 @@
 import re
 import statistics
 
+file_name = 'times/pushblock_bevf.txt'
+
 # open and read the file
-with open('times/push_block.txt', 'r') as file:
+with open(file_name, 'r') as file:
     data_str = file.read()
 
 # extract numbers
 numbers = re.findall(r'^\d{1,4}$', data_str, re.MULTILINE)
 # convert to int
-numbers = [int(num)/5 for num in numbers]
+numbers = [int(num)/10 for num in numbers]
 
 mean = statistics.mean(numbers)
 variance = statistics.variance(numbers)
 std_dev = statistics.stdev(numbers)
 
+print("file:", file_name)
 print("Mean:", mean)
 print("Variance:", variance)
 print("Standard Deviation:", std_dev)
